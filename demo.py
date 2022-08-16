089a6702497944f12d5385c51de4c27b3bdaf7b6
alextimofeev <oleksandr.timofieiev@bringg.com>
2022-08-16 13:01:37 +0300

    A function added

--- demo.py
+++ demo.py
@@ -1,2 +1,13 @@
 print("Hello")
 print("Welcome to Telusko")
+
+class NewBlock:
+
+    def __init__(self, stat):
+        self.status = stat
+
+    def new_func(self):
+        return self.status
+
+block1 = NewBlock("updated")
+print(block1.new_func())
